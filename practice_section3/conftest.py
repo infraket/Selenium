e1262ce9fb1af1b88291570dbcc180781ece6e95
infraket <aveacvale@mail.ru>
2022-09-14 04:47:17 +0400

    up section 3,practice

--- practice_section3/conftest.py
+++ practice_section3/conftest.py
@@ -4,15 +4,35 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language: en,es or ru")
+                     help="Choose language: en, es or ru")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
+    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
+    browser = None
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
+    print("\nquit browser..")
     browser.quit()
+    # options = Options()
+    # options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    # browser = webdriver.Chrome(options=options)
+    # yield browser
+    # browser.quit()
